ec845db68b5cc145298ebb8533bad8d65d9d8d38
K.R.Kavin <krkavinjkr@gmail.com>
2026-04-07 10:20:26 +0530

    ready for deployment

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,15 +1,15 @@
 import axios from "axios"
 
-const axiosClient = axios.create({baseURL :"http://localhost:3000",});
-axiosClient.interceptors.request.use((config)=>{
+const axiosClient = axios.create({ baseURL: "https://digital-audit-trail-system.onrender.com", });
+axiosClient.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
-    if(token) config.headers.Authorization =`Bearer ${token}`;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 });
-axiosClient.interceptors.response.use((response)=> response ,(error)=>{
-    if(error.response?.status === 401 && window.location.pathname !=="/"){
+axiosClient.interceptors.response.use((response) => response, (error) => {
+    if (error.response?.status === 401 && window.location.pathname !== "/") {
         localStorage.removeItem("token");
-        window.location.href ="/";
+        window.location.href = "/";
     }
     return Promise.reject(error);
 })
